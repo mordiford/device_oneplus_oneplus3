73dc396f5b3078f21ed2740c9be922e9c63b8190
lindwurm <lindwurm.q@gmail.com>
2017-11-30 12:56:50 +0900

    Revert "op3: Update ifaa manager for v2 api"
    This reverts commit b223cb0ac524419a9833d1be8b5c3bc32cecca5f.

--- org.ifaa.android.manager/Android.mk
+++ org.ifaa.android.manager/Android.mk
@@ -22,6 +22,5 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE := org.ifaa.android.manager
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 
 include $(BUILD_JAVA_LIBRARY)
